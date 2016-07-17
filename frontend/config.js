1a4f7f0f99be133ce2a042ee0a2639af0ca0f8c7
Chris Laganiere <ChrisLaganiere@gmail.com>
2016-07-17 15:12:58 -0700

    redirect to localhost

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'EVENTBRITE_OAUTH_URI': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=XTYHNKA4CXM73VGSI4',
-  'EVENTBRITE_CLIENT_KEY': 'XTYHNKA4CXM73VGSI4'
+  'EVENTBRITE_OAUTH_URI': 'https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=L7GAOSKH625YGDIFG3',
+  'EVENTBRITE_CLIENT_KEY': 'L7GAOSKH625YGDIFG3'
 }
